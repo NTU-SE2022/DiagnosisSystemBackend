d96fac3b43d9edcedb3c59ce8fc5bd779f6452e3
WeiJ Liu <g23988@gmail.com>
2022-12-10 21:48:57 +0800

    Update SymptomController.java

--- src/main/java/com/group3/DiagnosisSystemBackend/controller/SymptomController.java
+++ src/main/java/com/group3/DiagnosisSystemBackend/controller/SymptomController.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 import com.group3.DiagnosisSystemBackend.repository.SymptomRepository;
 
